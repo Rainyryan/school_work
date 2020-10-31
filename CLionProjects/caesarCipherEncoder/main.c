86c5f69ab7ad3478a623531a5a52e4d3caeb7aa9
Rainyryan <rc6150@gmail.com>
2020-11-01 04:13:03 +0800

    caesar update

--- CLionProjects/caesarCipherEncoder/main.c
+++ CLionProjects/caesarCipherEncoder/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    signed int k;
+    char s[100]={0};
+    scanf("%d\n",&k);
+    scanf("%[^\n]%*c",s);
+    //fgets(s,100,stdin);
+    k=k%26;
+    for(int i=0;i<=100;i++){
+        if((s[i]<=90&&s[i]>=65)){
+            s[i]+=k;
+            if(s[i]>90){s[i]-=26;}
+            if(s[i]<65){s[i]+=26;}
+        }
+        if((s[i]<=122&&s[i]>=97)){
+            s[i]+=k;
+            if(s[i]>122){s[i]-=26;}
+            if(s[i]<97){s[i]+=26;}
+        }
+        printf("%c",s[i]);
+    }
     return 0;
 }
