eb1215e823f52963d4976958efc0e87cba94108a
Rainyryan <rc6150@gmail.com>
2020-11-01 22:09:17 +0800

    caesar

--- CLionProjects/caesarCipherEncoder/main.c
+++ CLionProjects/caesarCipherEncoder/main.c
@@ -2,21 +2,22 @@
 
 int main(){
     int k;
-    char s[100]={0};
+    char s[200]={0};
     scanf("%d\n",&k);
-    scanf("%[^\n]%*c",s);
-    //fgets(s,100,stdin);
+    //scanf("%[^\n]%*c",s);
+    fgets(s,100,stdin);
     k=k%26;
-    for(int i=0;i<=100;i++){
-        if((s[i]<=90&&s[i]>=65)){
+    //printf("%d\n",k);
+    for( int i = 0; s[i] != '\n' ; i++){
+        if((s[i]<='Z'&&s[i]>='A')){
             s[i]+=k;
-            if(s[i]>90){s[i]-=26;}
-            if(s[i]<65){s[i]+=26;}
+            if(s[i]>'Z'){s[i]-=26;}
+            if(s[i]<'A'){s[i]+=26;}
         }
-        if((s[i]<=122&&s[i]>=97)){
+        if((s[i]<='z'&&s[i]>='a')){
             s[i]+=k;
-            if(s[i]>122){s[i]-=26;}
-            if(s[i]<97){s[i]+=26;}
+            if(s[i]>'z'){s[i]-=26;}
+            if(s[i]<'a'){s[i]+=26;}
         }
         printf("%c",s[i]);
     }
