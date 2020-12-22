7994edc3f532a7a2f2d0f1c0fd89762e6304baca
Rainyryan <rc6150@gmail.com>
2020-12-22 22:52:54 +0800

    good

---
+++ CLionProjects/Rat in a Maze/cmake-build-debug/CMakeFiles/Rat_in_a_Maze.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rat_in_a_Maze.dir/main.c.obj"
+  "Rat_in_a_Maze.exe"
+  "Rat_in_a_Maze.exe.manifest"
+  "Rat_in_a_Maze.pdb"
+  "libRat_in_a_Maze.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rat_in_a_Maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CLionProjects/Spread the Color/cmake-build-debug/CMakeFiles/Spread_the_Color.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spread_the_Color.dir/main.c.obj"
+  "Spread_the_Color.exe"
+  "Spread_the_Color.exe.manifest"
+  "Spread_the_Color.pdb"
+  "libSpread_the_Color.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Spread_the_Color.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
