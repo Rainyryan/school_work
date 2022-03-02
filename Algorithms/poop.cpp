50c1556fe643c8abb25b8789d432440fcabdb278
Rainyryan <rc6150@gmail.com>
2022-03-03 03:27:32 +0800

    Q3 in the works

--- Algorithms/poop.cpp
+++ Algorithms/poop.cpp
@@ -1,25 +1,62 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    map<int ,int> Mem;
     
     int N, M;
     cin>>N>>M;
 
-    while(N--){
-        string M; 
-        int a, b;
+    unordered_map<int, int> Mem;
+    vector<int> V;
+    string C; 
+    int a, b;
 
-        cin>>M;
-        M[0] == 'g'? cin>>a: cin>>a>>b;
-        
+    while(M--){
+        cin>>C;
+        if(C[0] == 'g'){
+            cin>>a;
+            //Mem.count(a)? cout<<Mem[a]<<endl : cout<<"-1"<<endl;
+            if(Mem.count(a)){
+                cout<<Mem[a]<<endl;
+                for(auto it = V.cbegin(); it != V.end(); it++)
+                    if(*it == a)
+                        V.erase(it);
+                V.push_back(a);
 
-
+            }else{
+                cout<<"-1"<<endl;
+            }
+        }else{
+            cin>>a>>b;
+            /*if(Mem.size() > N){
+                Mem.erase(V.front());
+                V.erase(V.begin());
+            }*/
+            cout<<"where";
+            Mem[a] = b;
+            V.push_back(a);
+            
+            for(int i = 0; i < V.size(); i++)
+                cout<<" "<<i;
+        }
     }
 }
+
+
+
+/*
+            int tmp = Mem[a];
+            Mem.erase(a);
+            Mem.emplace(a,tmp);
+            
+            if(Mem.count(a)){
+                cout<<Mem[a]<<endl;
+            }else{
+                cout<<"-1"<<endl;
+            }
+*/
