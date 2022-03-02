4278637eabb6fc9283cd5993d5028eb61cf17b1c
Rainyryan <rc6150@gmail.com>
2022-03-03 01:04:27 +0800

    Q3 little bit

--- Algorithms/poop.cpp
+++ Algorithms/poop.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -6,5 +7,19 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    map<int ,int> Mem;
     
+    int N, M;
+    cin>>N>>M;
+
+    while(N--){
+        string M; 
+        int a, b;
+
+        cin>>M;
+        M[0] == 'g'? cin>>a: cin>>a>>b;
+        
+
+
+    }
 }
