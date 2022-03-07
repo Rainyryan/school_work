00191b0e35b87141c87a5e81d82e79a45f47701a
Ryan <63191069+Rainyryan@users.noreply.github.com>
2022-03-07 16:52:29 +0800

    W2Q1

---
+++ Algorithms/potato.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> V;
+
+int main(){
+    int N,a;
+    cin>>N;
+    V.reserve(N);
+    while(N--) V.push_back(a);
+    
+}
+
+int merge(vector<int>& V){
+    merge()
+}
+
+/*
+4
+4 3 2 1
+
+4 3|2 1
+3 4|1 2
+
+1 2 3 4
+
+6 
+4 5 10 4 7 1
+4 5 10|4 7 1
+4 5|10\4 7|1
+4 5 10|1 4 7
+
+
+
+*/
