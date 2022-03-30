6038863909c564b37e5c3568a3e37bf381386397
Ryan <63191069+Rainyryan@users.noreply.github.com>
2022-03-30 11:04:37 +0800

    Update frog.cpp

--- Algorithms/frog.cpp
+++ Algorithms/frog.cpp
@@ -7,22 +7,22 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int T, N, W, P, pp = 0, max = 0;
+    int T, N, W, P, pp = 0, m = 0;
     char S;
     cin>>T;
     for(int i = 0; i < T; ++i){
         cin>>N>>W;
         for(int j = 0; j < N; ++j){
             cin>>S>>P;
-            if(S == 'b' && P - pp > max){
-                max = P;
+            if(S == 'b' && P - pp > m){
+                m = P;
                 pp = P;
             }
         }
-        max = std::max(max, W - pp);
-        cout<<max<<'\n';
+        m = max(m, W - pp);
+        cout<<m<<'\n';
         pp = 0;
-        max = 0;
+        m = 0;
     }
 }
 
@@ -33,7 +33,7 @@ b 5
 1 10 
 s 5 
 2 10 
-b 3 s 6 
+b 3 s 6
 3 20 
 s 5 b 10 s 16
 
