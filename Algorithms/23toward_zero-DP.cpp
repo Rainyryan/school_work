12382c8b992420b6ebbd2d7f33cd0737bd2ea838
ryan <rc6150@gmail.com>
2022-05-04 02:24:05 +0800

    optimized alg q23

--- Algorithms/23toward_zero-DP.cpp
+++ Algorithms/23toward_zero-DP.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int findMin(int arr[], int n){
     int sum = 0;
     int i, j;
-    for(int i = 0; i < n; i++) sum += arr[i];
+    for(i = 0; i < n; i++) sum += arr[i];
 
     int y = sum/2 + 1;
     bool dp[y];
-    for(int i = 0; i < y; i++) dp[i] = 0;
+    for(i = 0; i < y; i++) dp[i] = 0;
 
-    for(int i = 0; i < n; i++)
-        for(int j = y - 1; j >= arr[i]; j--)
+    for(i = 0; i < n; i++)
+        for(j = y - 1; j >= arr[i]; j--)
             if(dp[j - arr[i]] || j == arr[i]) dp[j] = true;
      
-    for(int i = y - 1; i >= 0; i--) if(dp[i]) return (sum - 2 * i);
+    for(i = y - 1; i >= 0; i--) if(dp[i]) return (sum - 2 * i);
 }
 
 int main(){
