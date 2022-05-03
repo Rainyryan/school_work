02d328c26af1a9e67a7ec0b24c20c616795c164d
Ryan <63191069+Rainyryan@users.noreply.github.com>
2022-05-04 02:14:43 +0800

    finally finished q23

--- Algorithms/23toward_zero-DP.cpp
+++ Algorithms/23toward_zero-DP.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
+int findMin(int arr[], int n){
+    int sum = 0;
+    int i, j;
+    for(int i = 0; i < n; i++) sum += arr[i];
+
+    int y = sum/2 + 1;
+    bool dp[y];
+    for(int i = 0; i < y; i++) dp[i] = 0;
+
+    for(int i = 0; i < n; i++)
+        for(int j = y - 1; j >= arr[i]; j--)
+            if(dp[j - arr[i]] || j == arr[i]) dp[j] = true;
+     
+    for(int i = y - 1; i >= 0; i--) if(dp[i]) return (sum - 2 * i);
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int n;
+    int n = 0;
     cin>>n;
-    int *arr = new int[n];
+    int *arr = new int[n]();
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
+    
     cout<<findMin(arr, n);
-    delete [] arr;
+    delete[] arr;
 }
