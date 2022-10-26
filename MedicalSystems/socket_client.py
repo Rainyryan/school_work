f87459b87a5d26107be2f2795f964f1104d6bcb9
Ryan <63191069+Rainyryan@users.noreply.github.com>
2022-10-26 22:55:23 +0800

    stuff

--- MedicalSystems/socket_client.py
+++ MedicalSystems/socket_client.py
@@ -1,10 +1,11 @@
 import socket
 
-HOST = '192.168.208.145'
-PORT = 50
-q = input("Please enter question:") + '?'
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(q.encode())
-    data = s.recv(1024).decode()
-print('Clent "" recirved ', repr(data))
+HOST = '192.168.1.109'
+PORT = 69
+while(1):
+    q = input("Please enter question:") + '?'
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(q.encode())
+        data = s.recv(1024).decode()
+        print('Clent "" recirved ', repr(data))
